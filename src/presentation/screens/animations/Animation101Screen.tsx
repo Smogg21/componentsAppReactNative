0b2bfd6fbc1a271d374736637f63ed6366ad6ea3
Smogg21 <spazzaglia98@gmail.com>
2024-08-16 12:32:19 -0500

    Made changes to animation

--- src/presentation/screens/animations/Animation101Screen.tsx
+++ src/presentation/screens/animations/Animation101Screen.tsx
@@ -1,27 +1,48 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useRef} from 'react';
-import {Animated, Pressable, StyleSheet, Text, View} from 'react-native';
+import {
+  Animated,
+  Easing,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {colors} from '../../../config/theme/theme';
 
 export const Animation101Screen = () => {
   const animatedOpacity = useRef(new Animated.Value(0)).current;
+  const animatedTop = useRef(new Animated.Value(-100)).current;
+
   const fadeIn = () => {
+    Animated.timing(animatedTop, {
+      toValue: 0,
+      duration: 700,
+      useNativeDriver: true,
+      easing: Easing.bounce,
+    }).start(() => console.log('animation ended'));
     Animated.timing(animatedOpacity, {
       toValue: 1,
       duration: 300,
       useNativeDriver: true,
     }).start(() => console.log('animation ended'));
   };
+
   const fadeOut = () => {
     Animated.timing(animatedOpacity, {
       toValue: 0,
       duration: 300,
       useNativeDriver: true,
-    }).start(() => console.log('animation ended'));
+    }).start(() => animatedTop.resetAnimation());
   };
   return (
     <View style={styles.container}>
-      <Animated.View style={[styles.purpleBox, {opacity: animatedOpacity}]} />
+      <Animated.View
+        style={[
+          styles.purpleBox,
+          {opacity: animatedOpacity, transform: [{translateY: animatedTop}]},
+        ]}
+      />
       <Pressable onPress={fadeIn} style={{marginTop: 10}}>
         <Text>FadeIn</Text>
       </Pressable>
