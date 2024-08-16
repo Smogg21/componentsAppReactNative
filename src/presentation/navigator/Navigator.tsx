a8bf250ab2c25ee8869355cc703059b2b71bb392
Smogg21 <spazzaglia98@gmail.com>
2024-08-16 11:32:06 -0500

    Worked on Animation101Screen

--- src/presentation/navigator/Navigator.tsx
+++ src/presentation/navigator/Navigator.tsx
@@ -1,6 +1,7 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import {HomeScreen} from '../screens/home/HomeScreen';
 import React from 'react';
+import {Animation101Screen} from '../screens/animations/Animation101Screen';
 
 const Stack = createStackNavigator();
 
@@ -8,6 +9,7 @@ export const Navigator = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="Animation101Screen" component={Animation101Screen} />
     </Stack.Navigator>
   );
 };
