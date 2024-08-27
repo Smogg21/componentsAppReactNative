d6ca81143cf73f13b875b864833151029b733551
Smogg21 <spazzaglia98@gmail.com>
2024-08-27 17:30:53 -0500

    Worked on separator component

--- src/presentation/screens/switches/SwitchScreen.tsx
+++ src/presentation/screens/switches/SwitchScreen.tsx
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import {CustomView} from '../../components/ui/CustomView';
 import {Card} from '../../components/ui/Card';
 import {CustomSwitch} from '../../components/ui/CustomSwitch';
+import {Separator} from '../../components/ui/Separator';
 
 export const SwitchScreen = () => {
   const [state, setState] = useState({
@@ -18,11 +19,13 @@ export const SwitchScreen = () => {
           onChange={value => setState({...state, isActive: value})}
           text="Está activo?"
         />
+        <Separator />
         <CustomSwitch
           isOn={state.isHungry}
           onChange={value => setState({...state, isHungry: value})}
           text="Tiene hambre?"
         />
+        <Separator />
         <CustomSwitch
           isOn={state.isHappy}
           onChange={value => setState({...state, isHappy: value})}
