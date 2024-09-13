e82d288593ac6cb6bac406bc7bf3da6d540a59b9
Smogg21 <spazzaglia98@gmail.com>
2024-09-13 11:07:50 -0500

    Finished input screens

--- src/presentation/screens/inputs/TextInputScreen.tsx
+++ src/presentation/screens/inputs/TextInputScreen.tsx
@@ -3,7 +3,13 @@ import React, {useState} from 'react';
 import {CustomView} from '../../components/ui/CustomView';
 import {Title} from '../../components/ui/Title';
 import {Card} from '../../components/ui/Card';
-import {TextInput, View} from 'react-native';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TextInput,
+  View,
+} from 'react-native';
 import {globalStyles} from '../../../config/theme/theme';
 import {Text} from 'react-native';
 
@@ -15,35 +21,59 @@ export const TextInputScreen = () => {
   });
 
   return (
-    <CustomView margin>
-      <Title text="Text Inputs" safe />
-      <Card>
-        <TextInput
-          style={globalStyles.input}
-          placeholder="Nombre completo"
-          autoCapitalize={'words'}
-          autoCorrect={false}
-          onChangeText={value => setForm({...form, name: value})}
-        />
-        <TextInput
-          style={globalStyles.input}
-          placeholder="Correo electrónico"
-          autoCapitalize={'none'}
-          autoCorrect={false}
-          keyboardType="email-address"
-          onChangeText={value => setForm({...form, email: value})}
-        />
-        <TextInput
-          style={globalStyles.input}
-          placeholder="Teléfono"
-          keyboardType="phone-pad"
-          onChangeText={value => setForm({...form, phone: value})}
-        />
-      </Card>
-      <View style={{height: 10}} />
-      <Card>
-        <Text>{JSON.stringify(form, null, 2)}</Text>
-      </Card>
-    </CustomView>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <ScrollView>
+        <CustomView margin>
+          <Title text="Text Inputs" safe />
+          <Card>
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Nombre completo"
+              autoCapitalize={'words'}
+              autoCorrect={false}
+              onChangeText={value => setForm({...form, name: value})}
+            />
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Correo electrónico"
+              autoCapitalize={'none'}
+              autoCorrect={false}
+              keyboardType="email-address"
+              onChangeText={value => setForm({...form, email: value})}
+            />
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Teléfono"
+              keyboardType="phone-pad"
+              onChangeText={value => setForm({...form, phone: value})}
+            />
+          </Card>
+          <View style={{height: 10}} />
+          <Card>
+            <Text>{JSON.stringify(form, null, 2)}</Text>
+            <Text>{JSON.stringify(form, null, 2)}</Text>
+            <Text>{JSON.stringify(form, null, 2)}</Text>
+            <Text>{JSON.stringify(form, null, 2)}</Text>
+            <Text>{JSON.stringify(form, null, 2)}</Text>
+            <Text>{JSON.stringify(form, null, 2)}</Text>
+            <Text>{JSON.stringify(form, null, 2)}</Text>
+            <Text>{JSON.stringify(form, null, 2)}</Text>
+            <Text>{JSON.stringify(form, null, 2)}</Text>
+            <Text>{JSON.stringify(form, null, 2)}</Text>
+          </Card>
+          <View style={{height: 50}} />
+          <Card>
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Teléfono"
+              keyboardType="phone-pad"
+              onChangeText={value => setForm({...form, phone: value})}
+            />
+          </Card>
+        </CustomView>
+        <View style={{height: 20}} />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
