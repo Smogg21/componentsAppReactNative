b1ef6621fe33d19486f16e301c01b4e11086a6ba
Smogg21 <spazzaglia98@gmail.com>
2024-09-17 17:04:18 -0500

    Worked on infinite Scroll component

--- src/presentation/screens/ui/InfiniteScrollScreen.tsx
+++ src/presentation/screens/ui/InfiniteScrollScreen.tsx
@@ -1,17 +1,47 @@
+/* eslint-disable react/no-unstable-nested-components */
+/* eslint-disable react-native/no-inline-styles */
 import React, {useState} from 'react';
-import {Text, View} from 'react-native';
-import {CustomView} from '../../components/ui/CustomView';
-import {Title} from '../../components/ui/Title';
+import {ActivityIndicator, Image, View} from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
+import {colors} from '../../../config/theme/theme';
 
 export const InfiniteScrollScreen = () => {
-  const [number, setNumber] = useState([0, 1, 2, 3, 4, 5]);
-  const loadMore = () => {};
+  const [numbers, setNumbers] = useState([0, 1, 2, 3, 4, 5]);
+
+  const loadMore = () => {
+    const newArray = Array.from({length: 5}, (_, i) => numbers.length + i);
+    setTimeout(() => {
+      setNumbers([...numbers, ...newArray]);
+    }, 3000);
+  };
 
   return (
-    <CustomView margin>
-      <Title text="Infinite Scroll" safe />
-      <FlatList />
-    </CustomView>
+    <View style={{backgroundColor: 'black'}}>
+      <FlatList
+        data={numbers}
+        onEndReached={loadMore}
+        onEndReachedThreshold={0.6}
+        keyExtractor={item => item.toString()}
+        renderItem={({item}) => <ListItem number={item} />}
+        ListFooterComponent={() => (
+          <View style={{height: 150, justifyContent: 'center'}}>
+            <ActivityIndicator size={40} color={colors.primary} />
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+interface ListItemProps {
+  number: number;
+}
+
+const ListItem = ({number}: ListItemProps) => {
+  return (
+    <Image
+      source={{uri: `https://picsum.photos/id/${number}/500/400`}}
+      style={{width: '100%', height: 400}}
+    />
   );
 };
