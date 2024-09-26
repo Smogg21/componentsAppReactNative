4c2c82af388db2bcf44ee849b80357840f3d2047
Smogg21 <spazzaglia98@gmail.com>
2024-09-26 10:37:31 -0500

    Finished slides component

--- src/presentation/screens/ui/SlidesScreen.tsx
+++ src/presentation/screens/ui/SlidesScreen.tsx
@@ -1,13 +1,18 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import {
   Image,
   ImageSourcePropType,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  Text,
   useWindowDimensions,
   View,
 } from 'react-native';
-import {colors} from '../../../config/theme/theme';
+import {colors, globalStyles} from '../../../config/theme/theme';
 import {FlatList} from 'react-native-gesture-handler';
+import {Button} from '../../components/ui/Button';
+import {useNavigation} from '@react-navigation/native';
 
 interface Slide {
   title: string;
@@ -34,14 +39,47 @@ const items: Slide[] = [
 ];
 
 export const SlidesScreen = () => {
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+  const flatListRef = useRef<FlatList>(null);
+  const navigation = useNavigation();
+
+  const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const {contentOffset, layoutMeasurement} = event.nativeEvent;
+    const currentIndex = Math.floor(contentOffset.x / layoutMeasurement.width);
+    setCurrentSlideIndex(currentIndex > 0 ? currentIndex : 0);
+  };
+
+  const scrollToSlide = (index: number) => {
+    if (!flatListRef.current) return;
+    flatListRef.current.scrollToIndex({index: index, animated: true});
+  };
+
   return (
     <View style={{flex: 1, backgroundColor: colors.background}}>
       <FlatList
+        ref={flatListRef}
         data={items}
         keyExtractor={item => item.title}
         renderItem={({item}) => <SlideItem item={item} />}
         horizontal
+        pagingEnabled
+        scrollEnabled={false}
+        onScroll={onScroll}
       />
+
+      {currentSlideIndex === items.length - 1 ? (
+        <Button
+          text="Finalizar"
+          onPress={() => navigation.goBack()}
+          styles={{position: 'absolute', bottom: 60, right: 30, width: 100}}
+        />
+      ) : (
+        <Button
+          text="Siguiente"
+          styles={{position: 'absolute', bottom: 60, right: 30, width: 100}}
+          onPress={() => scrollToSlide(currentSlideIndex + 1)}
+        />
+      )}
     </View>
   );
 };
@@ -73,6 +111,8 @@ const SlideItem = ({item}: SlideItemProps) => {
           alignSelf: 'center',
         }}
       />
+      <Text style={[globalStyles.title, {color: colors.primary}]}>{title}</Text>
+      <Text style={{color: colors.text, marginTop: 20}}>{desc}</Text>
     </View>
   );
 };
